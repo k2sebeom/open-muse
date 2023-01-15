54b2707839366f6598cbdd554b4a7948caf5cbc8
k2sebeom <moses97@gmail.com>
2023-01-15 02:05:02 -0500

    feat: Add show page

---
+++ src/components/Profile.tsx
@@ -0,0 +1,54 @@
+import Image from "next/image";
+import React from "react";
+
+import NoteImage from '../assets/images/note.jpg'
+
+
+type ProfileProps = {
+    size?: number;
+    name?: string;
+}
+
+function Profile({ size = 50, name = "PRofile" }: ProfileProps) {
+    return (
+        <div className="profileContainer">
+            <div className="clipShape">
+            <Image
+                src={NoteImage}
+                alt='profile'
+                width={size}
+                height={size}
+            />
+            </div>
+            
+            <h2>{name}</h2>
+
+            <style jsx>{`
+                .clipShape {
+                    position: relative;
+                    overflow: hidden;
+                    background-color: red;
+                    border-radius: 50%;
+                    width: ${size}px;
+                    height: ${size}px;
+                    box-shadow: 4px 2px 6px black;
+                }
+                
+                .profileContainer {
+                    display: flex;
+                    flex-direction: column;
+                    align-items: center;
+                    margin: 10px;
+                }
+
+                h2 {
+                    margin-top: 10px;
+                    margin-bottom: 0px;
+                }
+            `}</style>
+
+        </div>
+    )
+}
+
+export default Profile;
---
+++ src/pages/[roomId].tsx
@@ -0,0 +1,96 @@
+import { Grid } from "@mui/material";
+import { NextPage } from "next";
+import Header from "../components/Header";
+import Profile from "../components/Profile";
+import RoundButton from "../components/RoundButton";
+
+const Stage = () => {
+    return (
+        <>
+            <div className='stage'>
+                <Profile />
+            </div>
+
+            <style jsx>{`
+                .stage {
+                    width: 50%;
+                    height: 20vh;
+                    background-color: gray;
+                    box-shadow: 10px 5px 5px black;
+
+                    display: flex;
+                    align-items: center;
+                    justify-content: center;
+                }
+            `}</style>
+        </>
+    )
+}
+
+const Audience = () => {
+    return (
+        <>
+            <Grid
+                sx={{
+                    pt: 10,
+                    px: 3
+                }}
+                container
+                spacing={{ xs: 3, md: 10}}
+                columns={{ xs: 4, sm: 5, md: 20}}    
+            >
+                {
+                    [1, 2, 3, 4,5, 6, 7, 9, 2, 2, 2, 2, 2, 2, 2].map((_, i) => ((
+                        <Grid item xs={1} md={2} key={`audience-${i}`}>
+                            <Profile />
+                        </Grid>
+                    )))
+                }
+                
+            </Grid>
+        </>
+    )
+}
+
+const RoomPage: NextPage = () => {
+
+    return (
+        <div>
+            <Header />
+            <Stage />
+            <Audience />
+
+            <div className="light">
+            </div>
+
+            <div className="controls">
+                <RoundButton title='Go on Stage' />
+            </div>
+
+            <style jsx>{`
+                div {
+                    display: flex;
+                    flex-direction: column;
+                    align-items: center;
+                }
+                .controls {
+                    position: fixed;
+                    bottom: 20px;
+                }
+
+                .light {
+                    background-color: #000;
+                    width: 100vw;
+                    height: 100vh;
+                    position: absolute;
+                    top: 0;
+                    left: 0;
+                    opacity: 0.2;
+                }
+            `}</style>
+        </div>
+    )
+}
+
+
+export default RoomPage;
