56c4d6f5f6902a8b3e7d82029540ec64ec7a3e40
k2sebeom <moses97@gmail.com>
2023-01-16 16:05:12 -0500

    feat: Add default head

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -9,6 +9,11 @@ const Layout = ({ children }: LayoutProps) => {
         <link rel="icon" href="/favicon.ico" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta name="theme-color" content="#000000" />
+
+        <title>OpenMuse | Perform at your will</title>
+        <meta property="og:title" content="OpenMuse | Audio-only Musical Performance" />
+        <meta property="og:description" content="Show your talents in virtual concerts" />
+        <meta property="og:image" content='https://uploads-ssl.webflow.com/62e9c64d4b368567d3527841/630058482da10f931a5b50d1_Screen%20Shot%202022-08-19%20at%2011.40.44%20PM-p-1600.png' />
       </Head>
       {children}
     </>
